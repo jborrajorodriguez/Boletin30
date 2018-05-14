c0308bdb3ae081692b75975dbb02be8b305ebe8e
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-14 09:18:58 +0200

    interfaz

---
+++ src/boletin30/IntegranteSeleccionFutbol.java
@@ -0,0 +1,13 @@
+package boletin30;
+
+/**
+ * @author {Juan Borrajo Rodriguez}
+ */
+public interface IntegranteSeleccionFutbol {
+    
+    public void concentrarse();
+    public void viajar();
+    public void entrenar();
+    public void jugarPartido();
+
+}
